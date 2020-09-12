aa4176b637aa5ebde9bf8cce746f26df6f6edb65
Dishy <dishysun@hotmail.com>
2020-09-12 00:39:59 -0400

    Code: Dice class

---
+++ PiratenKapern/src/main/java/entity/Dice.java
@@ -0,0 +1,40 @@
+package entity;
+
+import java.util.Random;
+
+public class Dice {
+	
+	public enum Face{
+		SKULL,
+		MONKEY,
+		PARROT,
+		KNIFE,
+		COIN,
+		DIAMOND;
+	}
+	
+	private Face face;
+	private Boolean lock;
+	
+	public Dice(){
+		this.face = Face.SKULL;
+		this.lock = false;
+	}
+	
+	public Face getFace() {return this.face;}
+	public Boolean isLock() {return lock;}
+	
+	public void roll() {
+		if (this.lock) return;
+		Random r = new Random();
+		this.face = Face.values()[r.nextInt(6)]; 
+	}
+	
+	public void lock() {this.lock = true;}
+	public void unlock() {this.lock = false;}
+	
+	public String toString() {
+		if (this.lock) return "["+this.face.toString()+"]";
+		return this.face.toString();
+	}
+}
