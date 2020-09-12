67f106c89417ec5e00e0fc67d4bfad4d99471db1
Dishy <dishysun@hotmail.com>
2020-09-12 14:04:03 -0400

    Refactoring: improve Normal theme

--- PiratenKapern/src/main/java/game/Theme/Normal.java
+++ PiratenKapern/src/main/java/game/Theme/Normal.java
@@ -10,9 +10,11 @@ public class Normal implements Theme{
 
 	@Override
 	public OneTurnScoreChange scoreCalculation(HashMap<Face, Integer> map, int skullsFromCard) {
-		if(map.get(Dice.Face.SKULL) > 2) return new OneTurnScoreChange(OneTurnScoreChange.Range.SELF, 0);
+		if(map.get(Dice.Face.SKULL) - skullsFromCard > 2) return new OneTurnScoreChange(OneTurnScoreChange.Range.SELF, 0);
 		int score = 0;
 		for (Dice.Face f : Dice.Face.values()) {
+			//TODO does 3 more skulls counts for score?
+			if (f == Dice.Face.SKULL) continue;
 			switch (map.get(f)) {
 				case 3:
 					score += 100;
@@ -29,16 +31,16 @@ public class Normal implements Theme{
 				case 7:
 					score += 2000;
 					break;
-				case 8:
-					score += 4000;
-					break;
-				case 9:
-					score += 4000;
-					break;
 				default: break;
 			}
+			if (map.get(f) > 7) score += 4000;
 			if (f == Dice.Face.COIN || f == Dice.Face.DIAMOND) score += 100 * map.get(f);
 		}
+		//check for Full Chest
+		if (map.get(Dice.Face.SKULL) == skullsFromCard &&
+			(map.get(Dice.Face.MONKEY) == 0 || map.get(Dice.Face.MONKEY) > 2) &&
+			(map.get(Dice.Face.PARROT) == 0 || map.get(Dice.Face.PARROT) > 2) &&
+			(map.get(Dice.Face.SWORD) == 0 || map.get(Dice.Face.SWORD) > 2))  score += 500;
 		return new OneTurnScoreChange(OneTurnScoreChange.Range.SELF, score);
 	}
 
