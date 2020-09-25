b307e979ab01294ca957a55fa87ac58ec9c5717b
Haiyue Sun <dishysun@hotmail.com>
2020-09-25 01:34:06 -0400

    Test:TurnTest   + test cases for chest feature   * change assertTrue() to proper assertEquals() etc.

--- PiratenKapern/src/test/java/test/game/TurnTest.java
+++ PiratenKapern/src/test/java/test/game/TurnTest.java
@@ -2,12 +2,14 @@ package test.game;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
 import java.util.ArrayList;
 
 import org.junit.Test;
 
 import entity.Dice;
 import entity.FortuneCard.*;
+import game.ChestException;
 import game.Turn;
 import game.Theme.*;
 
@@ -17,22 +19,22 @@ public class TurnTest {
 	public void test_Constructor() {
 		//initialize a turn, all attributes should be set to default
 		Turn t = new Turn();
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0); 		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0); 		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		for (Dice.Face f : Dice.Face.values()) {
 			//every face should be counted as 0 at the beginning of the turn
-			assertTrue(t.getTreasureInHand().get(f) == 0);
+			assertEquals((int)t.getTreasureInHand().get(f), 0);
 		}
-		assertTrue(t.getFortuneCard() == null); //Fortune Card should be set now
+		assertNull(t.getFortuneCard()); //Fortune Card should be set now
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null); //chest cannot be accessed unless get Treasure Chest fortune card
+		assertNull(t.getChest()); //chest cannot be accessed unless get Treasure Chest fortune card
 	}
 	
 	@Test
@@ -42,18 +44,18 @@ public class TurnTest {
 		t = new Turn();
 		FortuneCard card = new TreasureChest();
 		t.setCard(card);
-		assertTrue(t.getChest().size() == 0); 			//get access to chest
+		assertEquals(t.getChest().size(), 0); 			//get access to chest
 		assertTrue(t.getFortuneCard() instanceof TreasureChest);
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0); 		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0); 		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		for (Dice.Face f : Dice.Face.values()) {
 			//every face should be counted as 0 at the beginning of the turn
-			assertTrue(t.getTreasureInHand().get(f) == 0);
+			assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
@@ -68,23 +70,23 @@ public class TurnTest {
 		t = new Turn();
 		FortuneCard card = new Captain();
 		t.setCard(card);
-		assertTrue(t.getscoreMultiplier() == 2);		//scoreMultiplier = 2
+		assertEquals(t.getscoreMultiplier(), 2);		//scoreMultiplier = 2
 		assertTrue(t.getFortuneCard() instanceof Captain);
 			//others should not change
-		assertTrue(t.getskullRerollCount() == 0); 		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getskullRerollCount(), 0); 		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		for (Dice.Face f : Dice.Face.values()) {
 			//every face should be counted as 0 at the beginning of the turn
-			assertTrue(t.getTreasureInHand().get(f) == 0);
+			assertEquals((int)t.getTreasureInHand().get(f), 0);
 		}
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null); 
+		assertNull(t.getChest()); 
 	}
 	
 	@Test
@@ -94,24 +96,24 @@ public class TurnTest {
 		t = new Turn();
 		FortuneCard card = new Sorceress();
 		t.setCard(card);
-		assertTrue(t.getskullRerollCount() == 1);
+		assertEquals(t.getskullRerollCount(), 1);
 		assertTrue(t.getFortuneCard() instanceof Sorceress); //skullRerollCounter = 0
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
 		 		
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		for (Dice.Face f : Dice.Face.values()) {
 			//every face should be counted as 0 at the beginning of the turn
-			assertTrue(t.getTreasureInHand().get(f) == 0);
+			assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null);
+		assertNull(t.getChest());
 	}
 	
 	@Test
@@ -124,20 +126,20 @@ public class TurnTest {
 		assertTrue(t.getTheme() instanceof game.Theme.SeaBattle);
 		assertTrue(t.getFortuneCard() instanceof entity.FortuneCard.SeaBattle);
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0);		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0);		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		for (Dice.Face f : Dice.Face.values()) {
 			//every face should be counted as 0 at the beginning of the turn
-			assertTrue(t.getTreasureInHand().get(f) == 0);
+			assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null);
+		assertNull(t.getChest());
 	}
 	
 	@Test
@@ -148,14 +150,14 @@ public class TurnTest {
 		FortuneCard card = new Gold();
 		t.setCard(card);
 		for (Dice.Face f : Dice.Face.values()) {
-			if (f == Dice.Face.COIN) assertTrue(t.getTreasureInHand().get(f) == 1);
-			else assertTrue(t.getTreasureInHand().get(f) == 0);
+			if (f == Dice.Face.COIN) assertEquals((int)t.getTreasureInHand().get(f), 1);
+			else assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
 		assertTrue(t.getFortuneCard() instanceof Gold);
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0);		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0);		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
 		assertTrue(t.getHand().size() == 8);  //8 Dice
 		for(Dice d : t.getHand()) {
@@ -163,7 +165,7 @@ public class TurnTest {
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null);
+		assertNull(t.getChest());
 	}
 	
 	@Test
@@ -174,22 +176,22 @@ public class TurnTest {
 		FortuneCard card = new Diamond();
 		t.setCard(card);
 		for (Dice.Face f : Dice.Face.values()) {
-			if (f == Dice.Face.DIAMOND) assertTrue(t.getTreasureInHand().get(f) == 1);
-			else assertTrue(t.getTreasureInHand().get(f) == 0);
+			if (f == Dice.Face.DIAMOND) assertEquals((int)t.getTreasureInHand().get(f),1);
+			else assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
 		assertTrue(t.getFortuneCard() instanceof Diamond);
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0);		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0);		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null);
+		assertNull(t.getChest());
 	}
 	
 	@Test
@@ -202,20 +204,20 @@ public class TurnTest {
 		assertTrue(t.getTheme() instanceof game.Theme.MonkeyBusiness);
 		assertTrue(t.getFortuneCard() instanceof entity.FortuneCard.MonkeyBusiness);
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0);		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0);		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		for (Dice.Face f : Dice.Face.values()) {
 			//every face should be counted as 0 at the beginning of the turn
-			assertTrue(t.getTreasureInHand().get(f) == 0);
+			assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null);
+		assertNull(t.getChest());
 	}
 	
 	@Test
@@ -226,22 +228,22 @@ public class TurnTest {
 		FortuneCard card = new Skull(1);
 		t.setCard(card);
 		for (Dice.Face f : Dice.Face.values()) {
-			if (f == Dice.Face.SKULL) assertTrue(t.getTreasureInHand().get(f) == 1);
-			else assertTrue(t.getTreasureInHand().get(f) == 0);
+			if (f == Dice.Face.SKULL) assertEquals((int)t.getTreasureInHand().get(f),1);
+			else assertEquals((int)t.getTreasureInHand().get(f), 0);
 		} 
 		assertTrue(t.getFortuneCard() instanceof Skull);
 			//others should not change
-		assertTrue(t.getscoreMultiplier() == 1); 		//scoreMultiplier = 1
-		assertTrue(t.getskullRerollCount() == 0);		//skullRerollCounter = 0
-		assertTrue(t.getTreasureInHand().size() == 6); 	//6 faces
+		assertEquals(t.getscoreMultiplier(), 1); 		//scoreMultiplier = 1
+		assertEquals(t.getskullRerollCount(), 0);		//skullRerollCounter = 0
+		assertEquals(t.getTreasureInHand().size(), 6); 	//6 faces
 		assertTrue(t.getTheme() instanceof game.Theme.Normal);
-		assertTrue(t.getHand().size() == 8);  //8 Dice
+		assertEquals(t.getHand().size(), 8);  //8 Dice
 		for(Dice d : t.getHand()) {
 			// Face = SKULL, unlocked at initialization
 			assertTrue(d.getFace() == Dice.Face.SKULL);
 			assertFalse(d.isLock());
 		}
-		assertTrue(t.getChest() == null);
+		assertNull(t.getChest());
 	}
 	
 	/*4 case after first roll
@@ -375,6 +377,12 @@ public class TurnTest {
 			assertTrue(t.getTreasureInHand().get(Dice.Face.SWORD) == sword);
 			assertTrue(t.getTreasureInHand().get(Dice.Face.COIN) == coin);
 			assertTrue(t.getTreasureInHand().get(Dice.Face.DIAMOND) == diamond);
+			t.setCard(new entity.FortuneCard.Gold());
+			assertTrue(t.getTreasureInHand().get(Dice.Face.COIN) == coin+1);
+			t.setCard(new entity.FortuneCard.Diamond());
+			assertTrue(t.getTreasureInHand().get(Dice.Face.DIAMOND)==diamond+1);
+			t.setCard(new entity.FortuneCard.Skull(2));
+			assertTrue(t.getTreasureInHand().get(Dice.Face.SKULL) == skull+2);
 		}else {
 			assertTrue(t.getTreasureInHand().get(Dice.Face.SKULL) == 0);
 			assertTrue(t.getTreasureInHand().get(Dice.Face.MONKEY) == 0);
@@ -383,5 +391,113 @@ public class TurnTest {
 			assertTrue(t.getTreasureInHand().get(Dice.Face.COIN) == 0);
 			assertTrue(t.getTreasureInHand().get(Dice.Face.DIAMOND) == 0);
 		}
+	}
+	
+	@Test
+	public void test_Chest_Feature() {
+		Turn t = new Turn();
+		HashSet<Integer> index = new HashSet<Integer>();
+		index.add(8);
+		index.add(-1);
+		index.add(0);
+		index.add(1);
+		index.add(2);
+		index.add(3);
+		index.add(4);
+		index.add(5);
+		index.add(6);
+		try {
+			t.moveToChest(index);
+			fail("no Chest now");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 1);
+			assertEquals(t.getHand().size(), 8);
+			assertNull(t.getChest());
+		}
+		try {
+			t.moveToHand(index);
+			fail("no Chest now");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 1);
+			assertEquals(t.getHand().size(), 8);
+			assertNull(t.getChest());
+		}
+		
+		t.setCard(new TreasureChest());
+		try {
+			t.moveToChest(index);
+			fail("exceed Hand size");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 2);
+			assertEquals(t.getHand().size(), 8);
+			assertEquals(t.getChest().size(), 0);
+		};
+		try {
+			t.moveToHand(index);
+			fail("exceed Chest size");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 2);
+			assertEquals(t.getHand().size(), 8);
+			assertEquals(t.getChest().size(), 0);
+		};
+		
+		index.remove(6);
+		try {
+			t.moveToChest(index);
+			fail("invalid index");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 3);
+			assertEquals(t.getHand().size(), 8);
+			assertEquals(t.getChest().size(), 0);
+		};
+		index.remove(8);
+		try {
+			t.moveToChest(index);
+			fail("invalid index");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 3);
+			assertEquals(t.getHand().size(),8);
+			assertEquals(t.getChest().size() , 0);
+		};
+		
+		index.remove(-1);
+		try {
+			t.moveToChest(index);
+			fail("moving skulls");
+		}catch(game.ChestException e) {
+			assertEquals(e.getCode(), 4);
+			assertEquals(t.getHand().size(),8);
+			assertEquals(t.getChest().size(),0);
+		};
+		
+		index.clear();
+		ArrayList<Dice> ds = new ArrayList<Dice>();
+		for (int i =0; i < 8;i++) {
+			Dice d = new Dice();
+			d.roll();
+			if (d.getFace() != Dice.Face.SKULL) index.add(i);
+			ds.add(d);
+		}
+		t.reroll(ds);
+		try {
+			t.moveToChest(index);
+		} catch (ChestException e) {
+			fail("Should not get Exception");
+		}
+		assertEquals(t.getChest().size(),index.size());
+		assertEquals(t.getHand().size(), 8-index.size());
+		
+		index.clear();
+		for (int i = 0; i < t.getChest().size()-1; i++) {
+			index.add(i);
+		}
+		try {
+			t.moveToHand(index);
+		}catch(ChestException e) {
+			fail("Should not get Exception");
+		}
+		//have "i < size - 1"
+		assertEquals(t.getChest().size(),1);
+		assertEquals(t.getHand().size(), 7);
 	}
 }
