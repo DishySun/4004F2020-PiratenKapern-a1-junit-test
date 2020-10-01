c73545565716768ea41f6b39ca13230392df2a3e
Dishy <dishysun@hotmail.com>
2020-10-01 17:30:27 -0400

    Refactoring:   + server side now show score detail on score calculation

--- PiratenKapern/src/main/java/game/Theme/SkullIsland.java
+++ PiratenKapern/src/main/java/game/Theme/SkullIsland.java
@@ -10,6 +10,7 @@ public class SkullIsland implements Theme {
 
 	@Override
 	public OneTurnScoreChange scoreCalculation(HashMap<Face, Integer> map,int skullsFromCard) {
+		System.out.println("Skulls x" + map.get(Dice.Face.SKULL) + "in Skull Island -" + (map.get(Dice.Face.SKULL) * 100) + " for all other players");
 		return new OneTurnScoreChange(OneTurnScoreChange.Range.OTHER, map.get(Dice.Face.SKULL) * -100);
 	}
 
